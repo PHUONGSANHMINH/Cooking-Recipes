a35a719f5244c942080d762880f00befc389b5d8
khanh nhi <khanhnhi@MacBook-Air-cua-Khanh-Nhi.local>
2024-05-09 10:08:35 +0700

    Test

--- app/src/main/java/com/example/threadyoutube/MainActivity.kt
+++ app/src/main/java/com/example/threadyoutube/MainActivity.kt
@@ -39,7 +39,7 @@ fun MAMA() {
             .fillMaxSize()
             .background(Color.White)
     ){
-        Text(text = "Cố lên nha các bạn!!!")
+        Text(text = "Cố lên nha các bạn hihii!!!")
     }
 
 }
